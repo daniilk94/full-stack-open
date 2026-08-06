458a97c3546a82650220ea069455d3cc9a162643
daniil <2404752@edu.karelia.fi>
2026-08-06 13:44:07 +0300

    small fix

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log("Connecting to ", url, "...");
+console.log("Connecting to MongoDB...");
 mongoose
   .connect(url, { family: 4 })
   .then((result) => {
